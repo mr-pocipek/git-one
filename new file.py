7bfeedda46efee983a635a691404324fb7b8f601
Zaha <zaharsurma1@gmail.com>
2024-07-05 11:25:49 +0200

    changed the new py file

--- new file.py
+++ new file.py
@@ -1,1 +1,2 @@
+print('heyy bittch i relly fucking miss you. i wish everything went well with us, cant stop thinking about it. i cant forget you. you just the sweetest thing i have ever met. i wish you will love someone as much as i do you')
 print('byee')
