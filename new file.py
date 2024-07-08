65881e1922205b07119edab3286b11f4e44732dc
Zaha <zaharsurma1@gmail.com>
2024-07-08 12:24:48 +0200

    added woow bitch messege

--- new file.py
+++ new file.py
@@ -1,3 +1,4 @@
 print('heyy bittch i relly fucking miss you. i wish everything went well with us, cant stop thinking about it. i cant forget you. you just the sweetest thing i have ever met. i wish you will love someone as much as i do you')
 print('byee')
 print('suck it up')
+print('woow bitch')
